6f236704725aba3f3e4f735177ef99cae802bc99
Veso <Ambroza8888@gmail.com>
2019-11-12 00:03:27 -0800

    more

---
+++ Contains_duplicate.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+namespace Csharp
+{
+  class ContainDup
+  {
+    public static bool ContainDuplicate(int[] nums)
+    {
+      for(int i =0 ; i < nums.Length-1;i++)
+      {
+        for(int j = i+1; j < nums.Length;j++)
+        {
+          
+        }
+      }
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ namespace Csharp
     {
         static void Main(string[] args)
         {
-            int[] arr = {1,0,1,0,0,3,12,0};
+            int[] arr = {1,2,3,1};
 
             // TwoSum.TwosumO(arr,9);
             // System.Console.WriteLine(revINT.reverseINT(12345));
             // System.Console.WriteLine(reverseStrDollarSign.reverseSTR("dc,b$a"));
             // TwoSum.TwoSum2(arr,9);
             // ReverseString.ReverseSTR1("Hi My name is Andrei");
-            Move0.MoveZero2(arr);
+            ContainDup.ContainDuplicate(arr);
         }
     }
 }
