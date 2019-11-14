6d3829bf9501e1e82d31a1a6e198efc69153977b
Veso <Ambroza8888@gmail.com>
2019-11-14 09:16:57 -0800

    qw

---
+++ stacks.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace Csharp
+{
+  class Stack
+  {
+    
+  }
+  class Node
+  {
+    public int value;
+    public Node next; 
+    
+  }
+}
