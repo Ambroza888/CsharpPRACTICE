ba4c06dcba1b1531df34fcfa60359c090beb77a4
Veso <Ambroza8888@gmail.com>
2019-11-23 01:42:47 -0800

    insert_binary_tree

--- BinarySearchTreeImplement.cs
+++ BinarySearchTreeImplement.cs
@@ -9,11 +9,24 @@ namespace Csharp
     public int value;
     public TreeNode left;
     public TreeNode right;
-    TreeNode(int x)
+    public TreeNode(int x)
     {
       left = null;
       right = null;
       value = x;
+    }
+  }
+  class BinarySearchTree
+  {
+    public static TreeNode root;
+    BinarySearchTree(){ root = null;}
+    public static void insert(int value)
+    {
+      TreeNode newNode = new TreeNode(value);
+      if(root == null)
+      {
+        root = newNode;
+      }
     }
   }
 }
