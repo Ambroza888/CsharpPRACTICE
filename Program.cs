e4258c092182699fe111704b0296d438bf848873
Veso <Ambroza8888@gmail.com>
2019-11-27 10:45:56 -0800

    selfDividingNumbersDone

--- Program.cs
+++ Program.cs
@@ -10,16 +10,8 @@ namespace Csharp
             int[] arr = {1,2,3,4,5,6,7,10,8};
             int[] arr2 = {1,2,3};
             string[] t = {"gin","zen","gig","msg"};
-            BinarySearchTree yo = new BinarySearchTree();
-            yo.insert(9);
-            yo.insert(4);
-            yo.insert(6);
-            yo.insert(20);
-            yo.insert(170);
-            yo.insert(15);
-            yo.lookup(170);
-            SumSqrtRoot.solution(18);
-            ValidParenteses.solution("((()");
+            SelfDividingNumbers.solution(1,22);
+
 
             
         }
---
+++ SelfDividingNumbers.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Csharp
+{
+  class SelfDividingNumbers
+  {
+    public static IList<int> solution(int left, int right)
+    {
+      IList<int> result = new List<int>();
+
+      while(left <= right)
+      {
+        int number = left;
+        bool selfdeviding = true;
+
+        if(number % 10 == 0) selfdeviding = false;
+        else
+        {
+            while(number > 0)
+            {
+              int digit = number % 10;
+              if(digit == 0 || left % digit !=0)
+              {
+                selfdeviding = false;
+                break;
+              }
+              number = number / 10;
+            }
+        }
+        if(selfdeviding)
+        {
+          result.Add(left);
+        }
+      }
+      return result;
+    }
+  }
+}
