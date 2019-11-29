72718a19a841db4f119bec9f08ec0a36463092b3
Veso <Ambroza8888@gmail.com>
2019-11-29 14:19:09 -0800

    graph done

--- Graph.cs
+++ Graph.cs
@@ -23,6 +23,7 @@ namespace Csharp
     public void addEdge(string node1, string node2)
     {
       adjacentList[node1].Add(node2);
+      //because is undirected i have to connect it other way around (5)-----(1000) /// (5)<----->(1999) - directed
       adjacentList[node2].Add(node1);
     }
   }
