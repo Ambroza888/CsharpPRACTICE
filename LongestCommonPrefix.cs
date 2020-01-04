94ad0d7a07181632241fe122fe713be4826bc631
Veso <Ambroza8888@gmail.com>
2020-01-03 23:40:59 -0800

    LongestCommongPrefix done

--- LongestCommonPrefix.cs
+++ LongestCommonPrefix.cs
@@ -7,5 +7,10 @@ namespace Csharp
   {
     //Input: ["flower","flow","flight"]
     //Output: "fl"
+    public static string solution(string[] strs)
+    {
+      string prefix = "";
+      if(strs.Length == 0) return prefix;
+    }
   }
 }
