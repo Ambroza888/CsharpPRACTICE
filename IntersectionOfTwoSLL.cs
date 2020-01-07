dca2dd9af501fd0c8f51a1e79ec97d495e9a0d88
Veso <Ambroza8888@gmail.com>
2020-01-07 01:08:14 -0800

    almost done

--- IntersectionOfTwoSLL.cs
+++ IntersectionOfTwoSLL.cs
@@ -7,6 +7,14 @@ namespace Csharp
   {
     public static MyNode Solution(MyNode headA, MyNode headB)
     {
+      // I am going to run two runners from the begging to the end and find lenght of one SSL
+      // The way i will do it will run a runner to the end with counter and find the length of the both lists;
+      // after i find the length i will find the diffrent between the two lists and i will use the absolute method ABS
+      //because if A or b list is bigger i don't need negative number
+      // having the counterA and counterB know which list is longer and i will let runner go WHILE the diff == 0 
+      // after the runnerA or runnerB run to the node x-times i will have runnerA and ruunerB on equal distans
+      // covering the part with that if they don't get crossed will cover with statement if runnerA != runnerB
+      // let's do it !!!!! :))))
       if(headA == null || headB == null) return null;
 
       MyNode rA = headA;
@@ -27,11 +35,13 @@ namespace Csharp
       }
       int diff = Math.Abs(countA - countB);
       rA = headA;
+      rB = headB;
+
       if(countA > countB)
       {
         while(diff > 0)
         {
-          qweqwe
+
         }
       }
 
